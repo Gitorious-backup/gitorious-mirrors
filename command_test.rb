a323d598580692ee4226f23535ac19c5e709c4f5
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-10 13:12:13 +0000

    Add Command class

---
+++ command_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+
+class Command
+  GIT_INIT_COMMAND = 'git init --bare'
+
+  def initialize(original_command)
+    @action, @path = original_command.split(' ')
+  end
+
+  def execute
+    "/bin/sh -c mkdir #{repo_path} && cd #{repo_path} && #{GIT_INIT_COMMAND}"
+  end
+
+  private
+
+  def repo_path
+    "/var/git/#{@path}"
+  end
+end
+
+describe Command do
+  before do
+    @command = Command.new("create foo")
+  end
+
+  it 'works' do
+    assert_equal '/bin/sh -c mkdir /var/git/foo && cd /var/git/foo && git init --bare', @command.execute
+  end
+end
