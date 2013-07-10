76c235a70cce78ae2249e6362b702eaed160c750
Piotr Solnica <piotr.solnica@gmail.com>
2013-07-10 13:49:49 +0000

    Extract Command::Clone action

--- command_test.rb
+++ command_test.rb
@@ -1,8 +1,6 @@
 require 'minitest/autorun'
 
 class Command
-  GIT_CLONE_COMMAND = 'git clone'
-
   def initialize(original_command)
     @action, @path = original_command.split(' ')
   end
@@ -11,26 +9,38 @@ class Command
     if @action == 'init'
       Command::Init.new(@path, '/var/git').execute
     elsif @action == 'clone'
-      "#{GIT_CLONE_COMMAND} /var/git/source /var/git/target"
+      Command::Clone.new('source', 'target', '/var/git').execute
     end
   end
 end
 
 class Command::Init
-  GIT_INIT_COMMAND  = 'git init --bare'
+  GIT_COMMAND  = 'git init --bare'
 
   def initialize(repo_name, repo_root)
     @repo_name, @repo_root = repo_name, repo_root
   end
 
   def execute
-    "/bin/sh -c mkdir #{repo_path} && cd #{repo_path} && #{GIT_INIT_COMMAND}"
+    "/bin/sh -c mkdir #{repo_path} && cd #{repo_path} && #{GIT_COMMAND}"
   end
 
   private
 
   def repo_path
     File.join(@repo_root, @repo_name)
+  end
+end
+
+class Command::Clone
+  GIT_COMMAND = 'git clone'
+
+  def initialize(source, target, repo_root)
+    @source, @target, @repo_root = source, target, repo_root
+  end
+
+  def execute
+    "#{GIT_COMMAND} /var/git/#{@source} /var/git/#{@target}"
   end
 end
 
@@ -63,5 +73,15 @@ describe Command::Init do
 
   it 'executes git init command' do
     assert_equal '/bin/sh -c mkdir /var/git/foo && cd /var/git/foo && git init --bare', @action.execute
+  end
+end
+
+describe Command::Clone do
+  before do
+    @action = Command::Clone.new('source', 'target', '/var/git')
+  end
+
+  it 'executes git clone command' do
+    assert_equal 'git clone /var/git/source /var/git/target', @action.execute
   end
 end
